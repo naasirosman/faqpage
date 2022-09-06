37616f4c59ff3e21fbbc65fb4c13037a11c8c17c
naasirosmanzoro <naasir.osman@zoro.co.uk>
2022-09-06 13:27:38 +0100

    webpages working - unable to fetch FAQs from BA&CO

---
+++ faq.js
@@ -0,0 +1,25 @@
+let accountDetailsBtn = document.querySelector(".accountDetailsSVG")
+let currentOrdersBtn = document.querySelector(".currentOrdersSVG")
+let ordersReturnBtn = document.querySelector(".ordersReturnSVG")
+let businessAccountsBtn = document.querySelector(".businessAccountsSVG")
+let paymentBtn = document.querySelector(".paymentSVG")
+let zoroFeaturesBtn = document.querySelector(".zoroFeaturesSVG")
+
+accountDetailsBtn.onclick = function () {
+    location.href = "accountDetailsFaqPage.html";
+};
+currentOrdersBtn.onclick = function () {
+    location.href = "currentOrdersFaqPage.html";
+};
+ordersReturnBtn.onclick = function () {
+    location.href = "ordersReturnFaqPage.html";
+};
+businessAccountsBtn.onclick = function () {
+    location.href = "businessAccountsFaqPage.html";
+};
+paymentBtn.onclick = function () {
+    location.href = "paymentFaqPage.html";
+};
+zoroFeaturesBtn.onclick = function () {
+    location.href = "zoroFeaturesFaqPage.html";
+};
